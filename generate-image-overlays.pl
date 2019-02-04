1b31be45b9b1ac3f0dfc2d0fceec63608cc4bbb8
Barry Carter <carter.barry@gmail.com>
2019-02-03 17:10:17 -0700

    All tiles now work

--- generate-image-overlays.pl
+++ generate-image-overlays.pl
@@ -21,6 +21,14 @@ require "/usr/local/lib/bclib.pl";
 
 chdir("/home/user/20190125/YAMC/MapTiles")||die("Can't chdir");
 
+# declare arrays
+
+print << "MARK";
+var bounds = {};
+var tiles = {};
+MARK
+;
+
 # TODO: change -180 to -179.9999 if needed
 # TODO: create the bounds and tile arrays
 
@@ -30,6 +38,39 @@ $count = 0;
 
 for ($lat = 90; $lat > -90; $lat-=45) {
   for ($lon = -180; $lon < 0; $lon+=45) {
+
+    # google doesn't like -180
+    # one millionth of a degree ~ 11.1cm
+    # plon = printed longitude
+    if ($lon == -180) {$plon = -179.999999;} else {$plon = $lon;}
+
+    my($ne) = join(", ",($lat, $lon+45));
+    my($sw) = join(", ",($lat-45, $plon));
+
+    print << "MARK"
+
+bounds[$count] = new google.maps.LatLngBounds(
+ new google.maps.LatLng($sw), new google.maps.LatLng($ne));
+
+tiles[$count] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_$w.png", bounds[$count]);
+
+tiles[$count].setMap(map);
+
+MARK
+;
+    $w++;
+    $count++;
+  }
+}
+
+# same thing for eastern tiles
+
+$e = "00";
+
+for ($lat = 90; $lat > -90; $lat-=45) {
+  for ($lon = 0; $lon < 180; $lon+=45) {
+
     my($ne) = join(", ",($lat, $lon+45));
     my($sw) = join(", ",($lat-45, $lon));
 
@@ -38,12 +79,14 @@ for ($lat = 90; $lat > -90; $lat-=45) {
 bounds[$count] = new google.maps.LatLngBounds(
  new google.maps.LatLng($sw), new google.maps.LatLng($ne));
 
-tile[$count] = new google.maps.GroundOverlay(
- "http://terramapadventure.com/MapTiles/lcc_west_$w.png", bounds[$count]);
+tiles[$count] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_$e.png", bounds[$count]);
+
+tiles[$count].setMap(map);
 
 MARK
 ;
-    $w++;
+    $e++;
     $count++;
   }
 }
---
+++ maptiles.js
@@ -0,0 +1,290 @@
+var bounds = {};
+var tiles = {};
+
+bounds[0] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(45, -179.999999), new google.maps.LatLng(90, -135));
+
+tiles[0] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_00.png", bounds[0]);
+
+tiles[0].setMap(map);
+
+
+bounds[1] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(45, -135), new google.maps.LatLng(90, -90));
+
+tiles[1] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_01.png", bounds[1]);
+
+tiles[1].setMap(map);
+
+
+bounds[2] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(45, -90), new google.maps.LatLng(90, -45));
+
+tiles[2] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_02.png", bounds[2]);
+
+tiles[2].setMap(map);
+
+
+bounds[3] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(45, -45), new google.maps.LatLng(90, 0));
+
+tiles[3] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_03.png", bounds[3]);
+
+tiles[3].setMap(map);
+
+
+bounds[4] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(0, -179.999999), new google.maps.LatLng(45, -135));
+
+tiles[4] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_04.png", bounds[4]);
+
+tiles[4].setMap(map);
+
+
+bounds[5] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(0, -135), new google.maps.LatLng(45, -90));
+
+tiles[5] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_05.png", bounds[5]);
+
+tiles[5].setMap(map);
+
+
+bounds[6] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(0, -90), new google.maps.LatLng(45, -45));
+
+tiles[6] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_06.png", bounds[6]);
+
+tiles[6].setMap(map);
+
+
+bounds[7] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(0, -45), new google.maps.LatLng(45, 0));
+
+tiles[7] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_07.png", bounds[7]);
+
+tiles[7].setMap(map);
+
+
+bounds[8] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-45, -179.999999), new google.maps.LatLng(0, -135));
+
+tiles[8] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_08.png", bounds[8]);
+
+tiles[8].setMap(map);
+
+
+bounds[9] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-45, -135), new google.maps.LatLng(0, -90));
+
+tiles[9] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_09.png", bounds[9]);
+
+tiles[9].setMap(map);
+
+
+bounds[10] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-45, -90), new google.maps.LatLng(0, -45));
+
+tiles[10] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_10.png", bounds[10]);
+
+tiles[10].setMap(map);
+
+
+bounds[11] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-45, -45), new google.maps.LatLng(0, 0));
+
+tiles[11] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_11.png", bounds[11]);
+
+tiles[11].setMap(map);
+
+
+bounds[12] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-90, -179.999999), new google.maps.LatLng(-45, -135));
+
+tiles[12] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_12.png", bounds[12]);
+
+tiles[12].setMap(map);
+
+
+bounds[13] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-90, -135), new google.maps.LatLng(-45, -90));
+
+tiles[13] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_13.png", bounds[13]);
+
+tiles[13].setMap(map);
+
+
+bounds[14] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-90, -90), new google.maps.LatLng(-45, -45));
+
+tiles[14] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_14.png", bounds[14]);
+
+tiles[14].setMap(map);
+
+
+bounds[15] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-90, -45), new google.maps.LatLng(-45, 0));
+
+tiles[15] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_west_15.png", bounds[15]);
+
+tiles[15].setMap(map);
+
+
+bounds[16] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(45, 0), new google.maps.LatLng(90, 45));
+
+tiles[16] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_00.png", bounds[16]);
+
+tiles[16].setMap(map);
+
+
+bounds[17] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(45, 45), new google.maps.LatLng(90, 90));
+
+tiles[17] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_01.png", bounds[17]);
+
+tiles[17].setMap(map);
+
+
+bounds[18] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(45, 90), new google.maps.LatLng(90, 135));
+
+tiles[18] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_02.png", bounds[18]);
+
+tiles[18].setMap(map);
+
+
+bounds[19] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(45, 135), new google.maps.LatLng(90, 180));
+
+tiles[19] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_03.png", bounds[19]);
+
+tiles[19].setMap(map);
+
+
+bounds[20] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(0, 0), new google.maps.LatLng(45, 45));
+
+tiles[20] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_04.png", bounds[20]);
+
+tiles[20].setMap(map);
+
+
+bounds[21] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(0, 45), new google.maps.LatLng(45, 90));
+
+tiles[21] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_05.png", bounds[21]);
+
+tiles[21].setMap(map);
+
+
+bounds[22] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(0, 90), new google.maps.LatLng(45, 135));
+
+tiles[22] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_06.png", bounds[22]);
+
+tiles[22].setMap(map);
+
+
+bounds[23] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(0, 135), new google.maps.LatLng(45, 180));
+
+tiles[23] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_07.png", bounds[23]);
+
+tiles[23].setMap(map);
+
+
+bounds[24] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-45, 0), new google.maps.LatLng(0, 45));
+
+tiles[24] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_08.png", bounds[24]);
+
+tiles[24].setMap(map);
+
+
+bounds[25] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-45, 45), new google.maps.LatLng(0, 90));
+
+tiles[25] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_09.png", bounds[25]);
+
+tiles[25].setMap(map);
+
+
+bounds[26] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-45, 90), new google.maps.LatLng(0, 135));
+
+tiles[26] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_10.png", bounds[26]);
+
+tiles[26].setMap(map);
+
+
+bounds[27] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-45, 135), new google.maps.LatLng(0, 180));
+
+tiles[27] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_11.png", bounds[27]);
+
+tiles[27].setMap(map);
+
+
+bounds[28] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-90, 0), new google.maps.LatLng(-45, 45));
+
+tiles[28] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_12.png", bounds[28]);
+
+tiles[28].setMap(map);
+
+
+bounds[29] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-90, 45), new google.maps.LatLng(-45, 90));
+
+tiles[29] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_13.png", bounds[29]);
+
+tiles[29].setMap(map);
+
+
+bounds[30] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-90, 90), new google.maps.LatLng(-45, 135));
+
+tiles[30] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_14.png", bounds[30]);
+
+tiles[30].setMap(map);
+
+
+bounds[31] = new google.maps.LatLngBounds(
+ new google.maps.LatLng(-90, 135), new google.maps.LatLng(-45, 180));
+
+tiles[31] = new google.maps.GroundOverlay(
+ "http://terramapadventure.com/MapTiles/lcc_east_15.png", bounds[31]);
+
+tiles[31].setMap(map);
+
