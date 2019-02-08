20ad37a7c78c84fff6a2e737ce06d834cb2109d7
analogy47 <actweed47@gmail.com>
2019-02-08 17:25:09 -0500

    testing web sockets

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const express = require('express'); // import the express package
+var WebSocketServer = require('ws').Server;
+var http = require('http');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
 
-const server = express(); // creates the server
+app.use(express.static(__dirname + '/'));
 
-const port = process.env.PORT || 8080;
+var server = http.createServer(app);
+server.listen(port);
 
-// handle requests to the root of the api, the / route
-server.get('/', (req, res) => {
-	res.send('API running!');
+console.log('http server listening on %d', port);
+
+var wss = new WebSocketServer({ server: server });
+console.log('websocket server created');
+
+wss.on('connection', function(ws) {
+	var id = setInterval(function() {
+		ws.send(JSON.stringify(new Date()), function() {});
+	}, 1000);
+
+	console.log('websocket connection open');
+
+	ws.on('close', function() {
+		console.log('websocket connection close');
+		clearInterval(id);
+	});
 });
-
-// watch for connections on port 8080
-server.listen(port, () =>
-	console.log('Server running on http://localhost:8080')
-);
