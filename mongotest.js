670e6a29e667db5808bee9b39b133c8869b752aa
Barry Carter <carter.barry@gmail.com>
2019-02-10 18:02:33 -0700

    Separate file testing mongodb

---
+++ mongotest.js
@@ -0,0 +1,19 @@
+// mongo db testing
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = process.env.MONGODB_URI;
+
+MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connection established to', url);
+
+      // do some work here with the database.
+
+      //Close connection
+      db.close();
+    }
+  });
+
+
