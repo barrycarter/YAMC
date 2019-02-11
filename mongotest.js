f78fc6314f64a7a37891c76a6e234a67d242ef21
Barry Carter <carter.barry@gmail.com>
2019-02-10 20:10:59 -0700

    Mongo test working

--- mongotest.js
+++ mongotest.js
@@ -7,7 +7,17 @@ MongoClient.connect(url, function (err, db) {
     if (err) {
       console.log('Unable to connect to the mongoDB server. Error:', err);
     } else {
-      console.log('Connection established to', url);
+      console.log('Connection established to mongodb');
+
+      //      console.log(db);
+
+      var dbo = db.db("heroku_0k4fvcm4");
+      var query = {};
+
+      dbo.collection("users").find(query).toArray(function(err, result) {
+	  if (err) throw err;
+	  console.log(result);
+	});
 
       // do some work here with the database.
 
