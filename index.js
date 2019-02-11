0b869d3c6516685d99c43f0e25427dcc3c0b267f
Barry Carter <carter.barry@gmail.com>
2019-02-10 17:34:44 -0700

    Commenting out mongodb code that broke things

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var url = process.env.MONGODB_URI;
 
-MongoClient.connect(url, function (err, db) {
-    if (err) {
-      console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-      console.log('Connection established to', url);
+// MongoClient.connect(url, function (err, db) {
+//    if (err) {
+//      console.log('Unable to connect to the mongoDB server. Error:', err);
+//    } else {
+//      console.log('Connection established to', url);
 
       // do some work here with the database.
 
       //Close connection
-      db.close();
-    }
-  });
+//      db.close();
+//    }
+//  });
 
 app.use(express.static(__dirname + '/'));
 
