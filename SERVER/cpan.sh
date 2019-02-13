8e175fff77bd2967c82d8218852361acf6b74f6f
Barry Carter <carter.barry@gmail.com>
2019-02-12 18:44:37 -0700

    Setting up new server on digital ocean

---
+++ SERVER/cpan.sh
@@ -0,0 +1,9 @@
+# CPAN installs
+
+cpan Protocol::WebSocket Net/WebSocket/Server.pm Protocol/WebSocket/Handshake/Server.pm Digest::MD5 Digest/SHA1.pm Date::Parse Text::Unidecode Statistics/Distributions.pm Math/Round.pm Astro::Nova Astro/MoonPhase.pm JSON.pm
+
+
+
+
+
+
