a10529109f39f1b449d4b4fa0d5c423fc82e536b
Barry Carter <carter.barry@gmail.com>
2019-02-14 16:16:18 -0700

    Testing works

--- socketesting.pl
+++ socketesting.pl
@@ -1,15 +1,45 @@
 #!/bin/perl
 
+# convert the standard input (in the form "x=xval&y=yval&z=zval") into
+# a JSON string, write a TCL/Expect script, and run it
+
 require "/usr/local/lib/bclib.pl";
 
-$| = 1;
-
-open(A, "|ws ws://127.0.0.1:8000/")||die("Can't open, $!");
+# header of the TCL/Expect script we are outputting
+open(A, ">/tmp/socket.tcl");
+print A << "MARK";
+#!/bin/expect
+spawn ws ws://127.0.0.1:8000/
+MARK
+;
 
 while (<>) {
-  debug("SENDING: $_");
-  print A $_;
-  sleep(1);
+
+  chomp;
+
+  # ignore lines start with hash
+  if (/^\#/) {next;}
+
+  my(%hash) = parse_form($_);
+
+  my(@keys) = ();
+
+  # for each key/val pair (insane level of quoting required)
+  for $i (keys %hash) {push(@keys, qq%\\"$i\\": \\"$hash{$i}\\"%);}
+
+  my($keys) = join(", ", @keys);
+
+  print A "send \"{$keys}\\n\"\n";
+
+#  debug("HASH", %hash);
+
+  debug("KEYS", @keys);
+
 }
 
+# we never expect to see this
+print A "expect \"999\"\n";
+
 close(A);
+
+system("chmod +x /tmp/socket.tcl; /tmp/socket.tcl");
