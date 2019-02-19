ae80dd75730ed0ba9c06317d85f48187bffaab3f
Barry Carter <carter.barry@gmail.com>
2019-02-19 11:25:32 -0700

    Checkpoint save

--- generate-solar-map.pl
+++ generate-solar-map.pl
@@ -9,3 +9,26 @@ require "/usr/local/lib/bclib.pl";
 
 # I used 'split -l 6906` to split the original file into two pieces
 # before uploading it to github to avoid large file issues
+
+open(A, "bzcat /home/user/20190125/YAMC/Data/PVOUT-[12].asc.bz2|");
+
+while (<A>) {
+
+  my(@pts) = split(/\s+/, $_);
+
+  # ignore first few lines which have no useful data
+  unless (scalar(@pts) == 43200) {next;}
+
+  # determine colors for each pt (-9999 = NA = 0)
+  for $i (@pts) {
+    if ($i == -9999) {$i = 0;}
+
+    # for testing, 0 is boring
+    if ($i == 0) {next;}
+
+    # split into 0.48 chunks
+    my($level) = min(15, floor($i/0.48));
+    debug("$i -> $level");
+  }
+}
+
