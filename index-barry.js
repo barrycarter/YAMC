115b01e7133130651013e05bff67bb4c7304a11d
Barry Carter <carter.barry@gmail.com>
2019-03-01 22:10:56 -0700

    Using node.js for server side

--- index-barry.js
+++ index-barry.js
@@ -1,6 +1,7 @@
 var WebSocketServer = require('ws').Server;
 var http = require('http');
 var fs = require('fs');
+var util = require('util');
 var port = 5000;
 
 var server = http.createServer();
@@ -38,15 +39,80 @@ var fd = fs.openSync('game-lib.js', 'r');
 
 console.log(fd);
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('/sites/DB/yamc.db');
+
+// perform a query our sqlite3 db and return results
+
+function queryDB(query) {
+  db.all(query, function(err, res) {arr = res;});
+  return arr;
+}
+
+function queryDB2(query) {
+  return new Promise( (resolve, reject) =>
+   db.all(query, function(err, data) {
+    if (err) {return reject(err);} else {return resolve(data);}}));
+}
+
+const betterdball = util.promisify(db.all);
+
+console.log(betterdball("SELECT * FROM users"));
+
+
+function queryDB3(query) {
+  return new Promise( (resolve, reject) =>
+   db.all(query, function(err, data) {
+    if (err) {return reject(err);} else {return resolve(data);}}));
+}
+
+async function wrapper(query) {
+//     console.log(await queryDB2(query));
+     var ret = await queryDB2(query);
+     return ret;
+}
+
+function queryDB4(db, query) {
+  return new Promise( (resolve, reject) =>
+   db.all(query, function(err, data) {
+    if (err) {return reject(err);} else {return resolve(data);}}));
+}
+
+function queryDB5(db, query) {
+   db.all(query, function(err, data) {
+    if (err) {return err;} else {return data;}
+});
+}
+
+function queryDB6(db, query, callback) {db.all(query, callback);}
+
+easyQuery = util.promisify(queryDB6);
+
+console.log(easyQuery(db, "SELECT * FROM users"));
+
+// queryDB2("SELECT * FROM users").then( result => console.log(result));
+
+// console.log(wrapper("SELECT * FROM users"));
+
+// queryDB2("SELECT * FROM users").then( result => {x=result;});
+
+// console.log(res);
+    
+
+// db.each("SELECT * FROM users", function(err, row) {
+//     console.log(row.username, row.variable, row.value);
+//   });
+
+
 // get the nth byte from a given file descriptor
 function byteFromFD(byte, fd) {
   fs.readSync(fd, buf, 0, 1, byte);
   return buf[0];
 }
 
-for (var i=0; i < 500; i++) {
-  console.log(byteFromFD(i, fd));
- }
+// for (var i=0; i < 500; i++) {
+//  console.log(byteFromFD(i, fd));
+// }
 
 // fs.readFile("game-lib.js", 'utf8', function(err, contents) {console.log(contents);});
 
---
+++ index-barry2.js
@@ -0,0 +1,62 @@
+var WebSocketServer = require('ws').Server;
+var http = require('http');
+var fs = require('fs');
+var util = require('util');
+var port = 5000;
+var sqlite3 = require('sqlite3').verbose();
+
+var server = http.createServer();
+server.listen(port);
+var wss = new WebSocketServer({ server: server });
+
+wss.on('connection', function(ws) {
+	var id = setInterval(function() {
+		ws.send(JSON.stringify(new Date()), function() {});
+	}, 1000);
+
+	ws.on('message', str => {
+		ws.send('hey');
+	});
+
+	console.log('websocket connection open');
+
+	ws.on('close', function() {
+		console.log('websocket connection close');
+		clearInterval(id);
+	});
+});
+
+// everything above this line is just to force JS into an infinite
+// loop, I don't actually use it right now
+
+// attempt to make the whole code one big async function
+
+var db = new sqlite3.Database('/sites/DB/yamc.db');
+// TODO: put actual file name here
+var fd = fs.openSync('game-lib.js', 'r');
+var buf = Buffer.alloc(1);
+
+async function the_whole_thing() {
+  var res = await queryDB("SELECT * FROM users");
+  var byte = await readByte(115);
+  console.log(res, byte);
+}
+
+the_whole_thing();
+
+function queryDB(query) {
+  return new Promise( (good, bad) => 
+   db.all(query, function(err, res) {good(res);}));
+}
+
+function readByte(n) {
+ return new Promise( (good, bad) => 
+  fs.read(fd, buf, 0, 1, n, function(err, bytes, buf) {good(buf);}));
+}
+
+
+
+
+
+
+
