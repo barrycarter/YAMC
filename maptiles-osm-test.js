2060404826e2a2ba669271dd96f838d4eef63adb
Barry Carter <carter.barry@gmail.com>
2019-03-20 07:33:17 -0600

    Checkpoint save

--- maptiles-osm-test.js
+++ maptiles-osm-test.js
@@ -1,18 +1,19 @@
 // TODO: why can't I do this
 // var ol = require('ol');
 
+
 function make_map() {
-var map = new ol.Map({
-target: 'map',
-layers: [
-new ol.layer.Tile({
-source: new ol.source.OSM()
-})
-],
-view: new ol.View({
-center: ol.proj.fromLonLat([37.41, 8.82]),
-zoom: 4
-})
-      })};
 
+console.log(ol);
 
+ var center = ol.proj.fromLonLat([-106.5, 35.05]);
+ var view = new ol.View({center: center, zoom: 4});
+ var layers = [new ol.layer.Tile({source: new ol.source.OSM()})];
+ var map = new ol.Map({target: 'map', view: view, layers: layers});
+ var bounds = new OpenLayers.Bounds(45, 135, 90, 180);
+
+ var graphic = new OpenLayers.ImageLayer({source: 'MapTiles/lcc-reduced-19.png'});
+
+ map.addLayers([graphic]);
+
+}
