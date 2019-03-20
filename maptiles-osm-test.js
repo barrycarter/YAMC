336f76f5586fa60f38988e3214186739c61059d9
Barry Carter <carter.barry@gmail.com>
2019-03-20 17:27:16 -0600

    Not really working, but checkpoint save

--- maptiles-osm-test.js
+++ maptiles-osm-test.js
@@ -1,7 +1,6 @@
 // TODO: why can't I do this
 // var ol = require('ol');
 
-
 function make_map() {
 
 console.log(ol);
@@ -10,7 +9,7 @@ console.log(ol);
  var view = new ol.View({center: center, zoom: 4});
  var layers = [new ol.layer.Tile({source: new ol.source.OSM()})];
  var map = new ol.Map({target: 'map', view: view, layers: layers});
- var bounds = new OpenLayers.Bounds(45, 135, 90, 180);
+ var bounds = new ol.Bounds(45, 135, 90, 180);
 
  var graphic = new OpenLayers.ImageLayer({source: 'MapTiles/lcc-reduced-19.png'});
 
