dbc7539d2aa3b417bdebb3f28edd8c66f78496a9
Barry Carter <carter.barry@gmail.com>
2019-05-01 13:25:28 -0600

    Getting a webserver going (minimal)

--- SERVER/yum.sh
+++ SERVER/yum.sh
@@ -1,3 +1,4 @@
 # yum installs
 
-yum -y install git bzip2 cpan gcc expect nodejs-ws golang yum-utils epel-release openssl openssl-devel certbot nmap-ncat nmap lsof ImageMagick
+yum -y install git bzip2 cpan gcc expect nodejs-ws golang yum-utils epel-release openssl openssl-devel certbot nmap-ncat nmap lsof ImageMagick nginx
+
