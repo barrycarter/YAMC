4867bd491503ab61f02024984eca9721d0d32832
analogy47 <actweed47@gmail.com>
2019-05-10 17:54:34 -0400

    uppdate app.js

--- map-client/src/App.js
+++ map-client/src/App.js
@@ -4,96 +4,84 @@ import './App.css'
 
 class App extends Component {
   state = {
-    // d: 0,
-    // m: 0,
-    // s: 0,
-    decimal: 0
-    // x: 0,
-    // y: 0
+    lat: 0,
+    lng: 0,
+    latDeg: 0,
+    latMin: 0,
+    latSec: 0,
+    lngDeg: 0,
+    lngMin: 0,
+    lngSec: 0,
+    x: 0,
+    y: 0
   }
 
   f = event => {
-    console.log('t', event.target.name)
-    if (event.target.name == 'decimal') {
-      console.log('hit dec')
+    if (event.target.name === 'decimal') {
       this.setState({
         decimal: event.target.value
       })
+    } else if (event.target.name === 'd') {
+      this.setState({
+        decimal: event.target.value
+      })
+    } else {
+      console.log('nothing')
     }
-    console.log('method', this.state)
-    console.log('v', event.target.value)
   }
 
   render () {
-    console.log('render', this.state)
     let d = Math.floor(this.state.decimal)
     let m = Math.floor((this.state.decimal - d) * 60)
-    let decimal = this.state.decimal
-    console.log(d)
-    console.log(this.state)
+    let s = Math.floor((this.state.decimal - d - m / 60) * 3600)
+    let x = 0
+    let y = 0
     return (
       <div className='App'>
         Input Box
         <div>
-          <form>
-            <input
-              onChange={this.f}
-              type='text'
-              value={this.state.decimal}
-              name='decimal'
-            />
-            <input type='text' value={d} name='d' />
-            <input type='text' value={m} name='m' />
-            <input type='text' value={this.state.s} name='s' />
-            <input type='text' value={this.state.x} name='x' />
-            <input type='text' value={this.state.y} name='y' />
-            {/* <button>Submit</button> */}
-          </form>
+          Latitude
+          <input
+            onChange={this.f}
+            type='text'
+            value={this.state.decimal}
+            name='decimal'
+          />
+          <input onChange={this.f} type='text' value={d} name='d' />
+          <input type='text' value={this.latDeg} name='m' />
+          <input type='text' value={this.latDeg} name='m' />
+          <input type='text' value={this.latMin} name='s' />
+          <input type='text' value={x} name='x' />
+          <input type='text' value={y} name='y' />
         </div>
         {this.state.decimal}
-        {/* DMS:
-        <TextThing size='2' name='d' obj={this} />
-        <TextThing size='2' name='m' obj={this} />
-        <TextThing size='2' name='s' obj={this} />
-        <p />
-        Decimal:
-        <TextThing size='8' name='decimal' obj={this} />
-        <p />
-        X:
-        <TextThing size='8' name='x' obj={this} />
-        Y:
-        <TextThing size='8' name='y' obj={this} /> */}
+        <div>
+          Longitude
+          <input
+            onChange={this.f}
+            type='text'
+            value={this.state.decimal}
+            name='decimal'
+          />
+          <input onChange={this.f} type='text' value={d} name='d' />
+          <input type='text' value={m} name='m' />
+          <input type='text' value={s} name='s' />
+          <input type='text' value={x} name='x' />
+          <input type='text' value={y} name='y' />
+        </div>
       </div>
     )
   }
+  // lat: 0,
+  // lng: 0,
+  // latDeg: 0,
+  // latMin: 0,
+  // latSec: 0,
+  // lngDeg: 0,
+  // lngMin: 0,
+  // lngSec: 0,
+  // x: 0,
+  // y: 0
 }
-
-// class TextThing extends Component {
-//   constructor (props) {
-//     super(props)
-//     this.state = {
-//       name: props.name,
-//       size: props.size,
-//       obj: props.obj,
-//       value: props.value
-//     }
-//     this.onChange.bind(this)
-//   }
-
-//   onChange (event) {
-//     this.setState({ value: event.target.value })
-//   }
-
-//   render () {
-//     return (
-//       <input
-//         name={this.state.name}
-//         size={this.state.size}
-//         value={this.state.value}
-//         onBlur={this.onChange}
-//       />
-//     )
-//   }
-// }
 
 export default App
