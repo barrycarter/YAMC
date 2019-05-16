01201d74551de01c07eb613db88821f497d02f94
analogy47 <actweed47@gmail.com>
2019-05-16 18:15:38 -0400

    add geoserver class

---
+++ MapServerTestingReal/geoserverLayer.js
@@ -0,0 +1,71 @@
+/** 
+May 16th, 2019
+Create layers for GeoServer: 
+get maps from GeoServer using 'tiled' mode → this is faster for both the client and the server.
+
+Constructor takes:
+  - GeoServerRoot: like "http://ws.terramapadventure.com:8080/geoserver/"
+  - layerName: the name of the layer
+
+Methods:
+
+- getFeature: returns a feature given a property and a latitude longitude as ffollows
+  - lat
+  - lng
+  - featureName
+
+*/
+
+class GeoServerLayer {
+	constructor(hash) {
+		this.hash = hash;
+	}
+
+	getFeature(hash) {
+		let bbox = [
+			hash.lng - 1 / 7200,
+			hash.lat - 1 / 7200,
+			hash.lng + 1 / 7200,
+			hash.lat + 1 / 7200,
+		].join(',');
+
+		let url = `${
+			this.hash.GeoServerRoot
+		}/wms?request=GetFeatureInfo&service=WMS&version=1.1.1&layers=${
+			this.hash.layerName
+		}&styles=&srs=EPSG%3A4326&format=image%2Fpng&bbox=${bbox}&width=1&height=1&query_layers=${
+			this.hash.layerName
+		}&info_format=application/json&propertyName=${
+			hash.featureName
+		}&feature_count=1&x=1&y=1`;
+
+		console.log(url);
+
+		let featureName = hash.featureName;
+
+		axios
+			.get(url)
+			.then(res => {
+				console.log(res.data.features[0].properties[hash.featureName]);
+			})
+			.catch(err => {
+				console.log('nope');
+			});
+	}
+}
+
+// const layer = new GeoServerLayer({
+// 	GeoServerRoot: 'http://ws.terramapadventure.com:8080/geoserver',
+// 	layerName: 'TMA-YAMC:ne_10m_admin_0_countries',
+// });
+
+// layer.getFeature({ lat: 35, lng: -106.5, featureName: 'NAME_EN' });
+
+// console.log(layer);
+
+/*
+
+
+http://ws.terramapadventure.com:8080/geoserver//wms?request=GetFeatureInfo&service=WMS&version=1.1.1&layers=TMA-YAMC:ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7&styles=&srs=EPSG%3A4326&format=image%2Fpng&bbox=-106,35,-105.999722222222,35.0002777777778&width=1&height=1&query_layers=TMA-YAMC:ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7&info_format=application/json&propertyName=GRAY_INDEX&feature_count=1&x=1&y=1
+
+*/
--- MapServerTestingReal/index.js
+++ MapServerTestingReal/index.js
@@ -1,60 +1,60 @@
-function query (e) {
-  console.log('button pressed')
+function query(e) {
+	console.log('button pressed');
 
-  let lat = parseFloat(document.getElementById('latitude').value)
-  let lng = parseFloat(document.getElementById('longitude').value)
+	let lat = parseFloat(document.getElementById('latitude').value);
+	let lng = parseFloat(document.getElementById('longitude').value);
 
-  let url = `http://ws.terramapadventure.com:8080/geoserver/wfs?service=wfs&version=2.0.0&request=GetPropertyValue&bbox=${lat -
-    1},${lng - 1},${lat + 1},${lng +
-    1}&typeNames=TMA-YAMC:ne_10m_admin_1_states_provinces&valueReference=name_en`
+	let url = `http://ws.terramapadventure.com:8080/geoserver/wfs?service=wfs&version=2.0.0&request=GetPropertyValue&bbox=${lat -
+		1},${lng - 1},${lat + 1},${lng +
+		1}&typeNames=TMA-YAMC:ne_10m_admin_1_states_provinces&valueReference=name_en`;
 
-  console.log(url)
+	console.log(url);
 
-  axios
-    .get(url)
-    .then(res => {
-      console.log(res.data)
-    })
-    .catch(err => {
-      console.log('nope')
-    })
+	axios
+		.get(url)
+		.then(res => {
+			console.log(res.data);
+		})
+		.catch(err => {
+			console.log('nope');
+		});
 
-  console.log(e, lat, lng)
+	console.log(e, lat, lng);
 }
 
-document.getElementById('submit').addEventListener('click', query)
+document.getElementById('submit').addEventListener('click', query);
 
-map = L.map('map', { crs: L.CRS.Simple })
-map.fitBounds([[-90, -180], [90, 180]])
-map.on('zoomend', redraw_map)
-map.on('moveend', redraw_map)
+// map = L.map('map', { crs: L.CRS.Simple })
+// map.fitBounds([[-90, -180], [90, 180]])
+// map.on('zoomend', redraw_map)
+// map.on('moveend', redraw_map)
 
-function redraw_map () {
-  // TODO: this is bad
+function redraw_map() {
+	// TODO: this is bad
 
-  map.eachLayer(function (layer) {
-    layer.remove()
-  })
-  map.eachLayer(function (layer) {
-    layer.remove()
-  })
+	map.eachLayer(function(layer) {
+		layer.remove();
+	});
+	map.eachLayer(function(layer) {
+		layer.remove();
+	});
 
-  let mapBounds = map.getBounds().toBBoxString()
+	let mapBounds = map.getBounds().toBBoxString();
 
-  // TODO: fix height and width
-  let url = `http://ows.terrestris.de/osm/service//wms?request=GetMap&service=WMS&version=1.1.1&layers=OSM-WMS&styles=default&srs=EPSG%3A4326&bbox=${mapBounds}&width=1600&height=800&format=image%2Fpng`
+	// TODO: fix height and width
+	let url = `http://ows.terrestris.de/osm/service//wms?request=GetMap&service=WMS&version=1.1.1&layers=OSM-WMS&styles=default&srs=EPSG%3A4326&bbox=${mapBounds}&width=1600&height=800&format=image%2Fpng`;
 
-  td(url, 'URL')
+	td(url, 'URL');
 
-  L.imageOverlay(url, map.getBounds(), {
-    opacity: 0.5
-  }).addTo(map)
+	L.imageOverlay(url, map.getBounds(), {
+		opacity: 0.5,
+	}).addTo(map);
 
-  url = `http://ws.terramapadventure.com:8080/geoserver/TMA-YAMC/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fjpeg&TRANSPARENT=true&LAYERS=TMA-YAMC%3ABeck_KG_V1_present_0p0083&exceptions=application%2Fvnd.ogc.se_inimage&SRS=EPSG%3A4326&STYLES=&WIDTH=1600&HEIGHT=900&BBOX=${mapBounds}`
+	url = `http://ws.terramapadventure.com:8080/geoserver/TMA-YAMC/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fjpeg&TRANSPARENT=true&LAYERS=TMA-YAMC%3ABeck_KG_V1_present_0p0083&exceptions=application%2Fvnd.ogc.se_inimage&SRS=EPSG%3A4326&STYLES=&WIDTH=1600&HEIGHT=900&BBOX=${mapBounds}`;
 
-  L.imageOverlay(url, map.getBounds(), {
-    opacity: 0.5
-  }).addTo(map)
+	L.imageOverlay(url, map.getBounds(), {
+		opacity: 0.5,
+	}).addTo(map);
 
-  // other interesting URLs
+	// other interesting URLs
 }
