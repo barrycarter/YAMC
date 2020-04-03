ada4c7956a415dba94f8c4f076e933638d1b43db
Barry Carter <carter.barry@gmail.com>
2020-04-03 12:56:09 -0600

    Checkpoint

--- SERVER/API/bc-api.pl
+++ SERVER/API/bc-api.pl
@@ -1,8 +1,36 @@
 #!/bin/perl
 
-print "Content-type: text/html\n\n";
+require "/usr/local/lib/bclib.pl";
 
-print "At the tone,", `date`;
+print "Access-Control-Allow-Origin: *\n";
 
-print "Have I broken my perms?\n";
+# TODO: restore to application/json
+# print "Content-type: application/json\n\n";
+
+print "Content-type: text/plain\n\n";
+
+my(%query) = str2hash($ENV{QUERY_STRING});
+
+if ($query{f} eq "time") {
+    print JSON::to_json(bcapi_time(%query));
+}
+
+# bcapi_time(%query);
+
+# print JSON::to_json(\%query);
+
+# for $i (sort keys %query) {
+#    print "$i: $query{$i}\n";
+# }
+
+# TODO: properly document this
+
+# gives time in a given timezone
+
+sub bcapi_time {
+
+    my(%hash) = @_;
+
+    return \%hash;
+}
 
